ebe0d234d61b8d65e392ba9261d413f2b1585416
johnsalemOT <john@octathorn.com>
2024-01-31 16:31:41 +0500

    Navbar updates

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -21,6 +21,7 @@ const Navbar = () => {
             background: "#FFF",
             boxShadow: "0px 1px 1.3px 0px rgba(0, 0, 0, 0.05)",
             padding: "16px 16px 16px 16px",
+            justifyContent: "space-between",
         },
         logo: {
             width: '130px',
@@ -29,7 +30,7 @@ const Navbar = () => {
         },
         tabs: {
             margin: 'auto',
-          
+            display: {xl: 'flex', lg: 'flex', md:'none', sm:'none', xs:'none'}
         },
         getTabColor: (tabIndex) => ({
             fontFamily: 'inherit',
@@ -38,14 +39,17 @@ const Navbar = () => {
             fontSize: '15px',
         }),
         search: {
-          display: {xl: 'flex', lg:'none', md: 'none'},
+          display: {xl: 'flex', lg:'none', md: 'flex'},
+        },
+        toolbar:{
+          justifyContent: 'space-between',
         }
     }
 
   return (
     <>
       <AppBar  position='static' sx={themeStyle.navbar}>
-        <Toolbar>
+        <Toolbar sx={themeStyle.toolbar}>
             <BuilderProNavbarLogo aria-label= "Builder Pro Logo" style={themeStyle.logo}/>
             <Tabs sx={themeStyle.tabs} value={selectedTab} onChange={handleTabChange} indicatorColor='' centered>
             <Tab label= 'Dashboard' style={themeStyle.getTabColor(0)}/>
@@ -58,8 +62,11 @@ const Navbar = () => {
             <Tab label='Subscription' style={themeStyle.getTabColor(5)}/>
             <Tab label='Settings' style={themeStyle.getTabColor(6)}/>
             </Tabs>
+            <Box>
+              
             <BuilderProButton backgroundColor={'#FFAC00'} variant={'contained'} Icon={BuilderProNavbarShare}>Share</BuilderProButton>
             <BuilderProButton backgroundColor={'#4C8AB1'} variant={'outlined'} Icon={BuilderProNavbarLogout}>Logout</BuilderProButton>
+            </Box>
         </Toolbar>
       </AppBar>
     </>
