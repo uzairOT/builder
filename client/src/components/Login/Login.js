be00157d0d1b898339cd174fe294408401841fb8
Raja Hassan <hassanejazrajah@gmail.com>
2024-07-12 17:44:23 +0500

    new changes and fixes

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -232,9 +232,10 @@ const Login = () => {
           >
             {" "}
             <img
+            
               src={googlePlay}
               // width={widthValue}
-              style={{ cursor: "pointer" }}
+              style={{ cursor: "pointer", height:62 }}
               alt=""
             />
           </a>
@@ -243,7 +244,7 @@ const Login = () => {
             <img
               src={appStore}
               width={widthValue}
-              style={{ cursor: "pointer" }}
+              style={{ cursor: "pointer", height:62 }}
               alt=""
             />
           </a>
@@ -500,13 +501,14 @@ const firstGrid = {
     sm: "0rem 2rem",
     xs: "0rem 0rem 0rem 0rem",
   },
+  alignItems:'start',
   backgroundColor: "#4C8AB1",
   // marginTop: { lg: "0rem", sm: "-1rem", xs: "0rem" },
   // border: "2px solid red",
 };
 
 const SecondGrid = {
-  gap: { lg: "1.1rem", sm: "1rem", xs: "1rem" },
+  gap: { xl:"4.5rem",lg: "3.5rem", sm: "1rem", xs: "1rem" },
   alignItems: { lg: "center", md: "center", sm: "center", xs: "center" },
   justifyContent: {
     lg: "start",
@@ -519,7 +521,7 @@ const SecondGrid = {
 };
 
 const downloadForMobBox = {
-  marginTop: "3rem",
+  // marginTop: "3rem",
   display: { lg: "flex", md: "flex", sm: "none", xs: "none" },
   // marginLeft: { lg: "2.5rem", md: "-1rem", sm: "-3rem" },
   justifyContent: "center",
@@ -529,7 +531,7 @@ const googleAppImgsBox = {
   display: { lg: "flex", md: "flex", sm: "none", xs: "none" },
   justifyContent: "center",
   alignItems: "center",
-  marginTop: "3rem",
+  // marginTop: "3rem",
   marginLeft: { lg: "3rem", md: "3rem", sm: "3rem" },
   gap: "1rem",
 };
@@ -537,7 +539,7 @@ const googleAppImgsMobile = {
   display: { lg: "none", md: "none", sm: "flex", xs: "none" },
   justifyContent: "center",
   alignItems: "center",
-  marginTop: "1rem",
+  // marginTop: "1rem",
   gap: "1rem",
   cursor: "pointer",
 };
--- client/src/pages/InvitationView/Invitation.js
+++ client/src/pages/InvitationView/Invitation.js
@@ -204,28 +204,34 @@ useEffect(() => {
   return (
     <Grid container sx={firstGrid}>
       <Grid item container lg={6} md={6} sm={12} xs={12} sx={SecondGrid}>
-      <Box
+      <img
+            style={{ height: "226px",width:"425px" }}
+            src={builderproicon}
+            alt="Builder Pro"
+          />
+        <Box
           sx={{
             display: "flex",
-            alignItems: "center",
             flexDirection: "column",
-            marginTop: { xl: "5rem", lg: "3rem", md: "2rem", sm: "0rem" },
+            marginTop: { xl: "0rem", lg: "0rem", md: "0rem", sm: "0rem" },
           }}
         >
-          <img style={{height:"100px"}} src={builderproicon} alt="Builder Pro" />
+         
           {/* <Typography sx={firstHeading}>BuilderBUILDER PRO</Typography> */}
-          <Typography component="p" pt={2} sx={secondHeading}>
-            On schedule.
-          </Typography>
-          <Typography component="p" sx={secondHeading}>
-            {" "}
-            On budget.{" "}
-          </Typography>
+          <Box>
+            <Typography component="p" pt={2} sx={secondHeading}>
+              On schedule.
+            </Typography>
+            <Typography component="p" sx={secondHeading}>
+              {" "}
+              On budget.{" "}
+            </Typography>
 
-          <Typography component="p" sx={secondHeading}>
-            {" "}
-            On the path to building better.
-          </Typography>
+            <Typography component="p" sx={secondHeading}>
+              {" "}
+              On the path to building better.
+            </Typography>
+          </Box>
         </Box>
           {/* <img height="55px" src={builderproicon} alt="Builder Pro" /> */}
           {/* <Typography sx={firstHeading}>BuilderBUILDER PRO</Typography> */}
@@ -234,12 +240,38 @@ useEffect(() => {
 
         
       
+       
         <Box sx={downloadForMobBox}>
-          <img src={downloadForMob} width={DoMobWidth} alt="" />
+          <img
+            src={downloadForMob}
+            width={'100%'}
+            alt=""
+            style={{ height: "143px" }}
+          />
         </Box>
         <Box sx={googleAppImgsBox}>
-          <img src={googlePlay} width={widthValue} alt="" />
-          <img src={appStore} width={widthValue} alt="" />
+        <a
+            href="https://play.google.com/store/apps?hl=en&gl=US&pli=1"
+            target="blank"
+          >
+            {" "}
+            <img
+            
+              src={googlePlay}
+              // width={widthValue}
+              style={{ cursor: "pointer", height:62 }}
+              alt=""
+            />
+          </a>
+
+          <a href="https://www.apple.com/store" target="blank">
+            <img
+              src={appStore}
+              width={widthValue}
+              style={{ cursor: "pointer", height:62 }}
+              alt=""
+            />
+          </a>
         </Box>
       </Grid>
       <Grid
@@ -573,7 +605,7 @@ const firstGrid = {
 };
 
 const SecondGrid = {
-  gap: { lg: "1.1rem", sm: "1rem", xs: "1rem" },
+  gap: { xl:"4.5rem",lg: "3.5rem", sm: "1rem", xs: "1rem" },
   alignItems: { lg: "center", md: "center", sm: "center", xs: "center" },
   justifyContent: {
     lg: "start",
@@ -587,17 +619,17 @@ const SecondGrid = {
 };
 
 const downloadForMobBox = {
-  marginTop: "1rem",
-  marginLeft: { lg: "2.5rem", md: "-1rem", sm: "-3rem" },
+  // marginTop: "3rem",
   display: { lg: "flex", md: "flex", sm: "none", xs: "none" },
+  // marginLeft: { lg: "2.5rem", md: "-1rem", sm: "-3rem" },
   justifyContent: "center",
   alignItems: "center",
 };
+
 const googleAppImgsBox = {
   display: { lg: "flex", md: "flex", sm: "none", xs: "none" },
   justifyContent: "center",
   alignItems: "center",
-  marginTop: "2rem",
   marginLeft: { lg: "0rem", md: "-3rem", sm: "-3rem" },
   gap: "1rem",
 };
@@ -722,13 +754,14 @@ const firstHeading = {
 };
 
 const secondHeading = {
-  textAlign:"center",
+  ml: 10,
+  textAlign:"Left",
   color: "rgba(255, 255, 255, 0.80)",
   // width: { lg: "31.125rem", md: "28rem", sm: "auto" },
   marginTop: "0.5rem",
   display: { lg: "flex", md: "flex", sm: "none", xs: "none" },
   fontFamily: "Arial Rounded MT, sans-serif",
-  fontSize: { lg: "2rem", md: "1rem", sm: "0.8rem" },
+  fontSize: { xl: "2rem", lg: "1.5rem", md: "1rem", sm: "1rem" },
   fontWeight: 400,
 };
 
